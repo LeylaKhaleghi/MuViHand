0b7d33bde62a99aa543927d8cb05cb6b9aca7a28
LeylaKhaleghi <65983447+LeylaKhaleghi@users.noreply.github.com>
2022-01-18 16:15:56 -0500

    Add files via upload

---
+++ Results/Plot_results_cross_subject.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Result on MuViHand dataset for cross-subject test protocol!
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# UPDATA the main root
+root = '/home/...'
+
+color_list = ['steelblue', 'orange', 'green', 'mediumorchid', 'violet', 'brown', 'lightsteelblue', 'aquamarine', 'palegoldenrod']
+
+All_threshold = np.load(root + 'All_threshold_cross_sub.npy')
+All_pck_curve = np.load(root +'All_pck_curve_cross_sub.npy')
+
+
+plt.figure()
+    
+plt.plot(All_threshold[0], All_pck_curve[0] ,  color=color_list[2], label='Boukhayma et al.' )
+plt.plot(All_threshold[1], All_pck_curve[1] ,  color=color_list[5],  label='Hasson et al.')
+plt.plot(All_threshold[2], All_pck_curve[2] , color=color_list[3],  label='Doosti et al.')
+plt.plot(All_threshold[3], All_pck_curve[3] , color=color_list[0], label='Baseline 2')
+plt.plot(All_threshold[4], All_pck_curve[4] , color=color_list[1],  label='Baseline 1')
+plt.plot(All_threshold[5], All_pck_curve[5] , color=color_list[6], label='MuViHandNet')
+
+
+plt.legend(loc='lower right',  facecolor="white")
+plt.xlabel("Threshold in mm")
+plt.title('cross-subject')
+plt.ylabel("PCK")
+plt.ylim(-0.05, 1.05)
+plt.xlim(-0.05, 50.05)
+
+plt.grid()
+
+# plt.savefig('../sub'+ '.pdf')
+plt.show()
+plt.clf()
